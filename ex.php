22416fa1d23497297c75bb60a74dd4319cb9f34c
ons94 <35642561+ons94@users.noreply.github.com>
2018-01-21 18:14:33 +0100

    Update ex.php

--- ex.php
+++ ex.php
@@ -1,11 +1,11 @@
 <?php 
 
-echo'<center>helllooo<h1></h1>'
+echo'Distance entre 2 points '
 
 function getDistance ($latitude1,$longitude1,$latitude2,$longitude2)
 {$earthradius=6371; 
 $latFrom=deg2rad($latitude1);
-$lonForm=deg2rad($longitude1);
+$lonFrom=deg2rad($longitude1);
 $latTo=deg2rad($latitude1);
 $lonTo=deg2rad($longitude2);
 
@@ -14,7 +14,9 @@ $lon=$lonTo-$lonFrom;
 
 $angle=2*asin(sqrt(pow(sin($lat/2),2)+cos($latfrom)*cos($latTo)*pow(sin($lon/2),2)));
 return $angle*$earthradius;
-
 }
+
+     $distance=getDistance($latitude1,$longitude1,$latitude2,$longitude2);
+     echo $distance;
 
 ?>
